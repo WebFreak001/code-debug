6fa64205906409a5e3b64e673a00320848cbb86b
dd86k <dd@dax.moe>
2024-08-08 12:21:08 -0400

    Add code to support Aliceserver

---
+++ src/aliceserver.ts
@@ -0,0 +1,130 @@
+import { MI2DebugSession, RunCommand } from './mibase';
+import { DebugSession, InitializedEvent, TerminatedEvent, StoppedEvent, OutputEvent, Thread, StackFrame, Scope, Source, Handles } from 'vscode-debugadapter';
+import { DebugProtocol } from 'vscode-debugprotocol';
+import { MI2_ALICE } from "./backend/mi2/mi2aliceserver";
+import { SSHArguments, ValuesFormattingMode } from './backend/backend';
+
+export interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
+	cwd: string;
+	target: string;
+	aliceserverpath: string;
+	env: any;
+	debugger_args: string[];
+	pathSubstitutions: { [index: string]: string };
+	arguments: string;
+	autorun: string[];
+	stopAtEntry: boolean | string;
+	ssh: SSHArguments;
+	valuesFormatting: ValuesFormattingMode;
+	printCalls: boolean;
+	showDevDebugOutput: boolean;
+}
+
+export interface AttachRequestArguments extends DebugProtocol.AttachRequestArguments {
+	cwd: string;
+	target: string;
+	aliceserverpath: string;
+	env: any;
+	debugger_args: string[];
+	pathSubstitutions: { [index: string]: string };
+	executable: string;
+	autorun: string[];
+	stopAtConnect: boolean;
+	stopAtEntry: boolean | string;
+	valuesFormatting: ValuesFormattingMode;
+	printCalls: boolean;
+	showDevDebugOutput: boolean;
+}
+
+class AliceserverDebugSession extends MI2DebugSession {
+	protected override initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+		response.body.supportsGotoTargetsRequest = true;
+		response.body.supportsHitConditionalBreakpoints = true;
+		response.body.supportsConfigurationDoneRequest = true;
+		response.body.supportsConditionalBreakpoints = true;
+		response.body.supportsFunctionBreakpoints = true;
+		response.body.supportsEvaluateForHovers = true;
+		this.sendResponse(response);
+	}
+
+	protected override launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): void {
+		const dbgCommand = args.aliceserverpath || "lldb-mi";
+		if (this.checkCommand(dbgCommand)) {
+			this.sendErrorResponse(response, 104, `Configured debugger ${dbgCommand} not found.`);
+			return;
+		}
+		this.miDebugger = new MI2_ALICE(dbgCommand, [], args.debugger_args, args.env);
+		this.setPathSubstitutions(args.pathSubstitutions);
+		this.initDebugger();
+		this.quit = false;
+		this.attached = false;
+		this.initialRunCommand = RunCommand.RUN;
+		this.isSSH = false;
+		this.started = false;
+		this.crashed = false;
+		this.setValuesFormattingMode(args.valuesFormatting);
+		this.miDebugger.printCalls = !!args.printCalls;
+		this.miDebugger.debugOutput = !!args.showDevDebugOutput;
+		this.stopAtEntry = args.stopAtEntry;
+		if (args.ssh !== undefined) {
+			if (args.ssh.forwardX11 === undefined)
+				args.ssh.forwardX11 = true;
+			if (args.ssh.port === undefined)
+				args.ssh.port = 22;
+			if (args.ssh.x11port === undefined)
+				args.ssh.x11port = 6000;
+			if (args.ssh.x11host === undefined)
+				args.ssh.x11host = "localhost";
+			if (args.ssh.remotex11screen === undefined)
+				args.ssh.remotex11screen = 0;
+			this.isSSH = true;
+			this.setSourceFileMap(args.ssh.sourceFileMap, args.ssh.cwd, args.cwd);
+			this.miDebugger.ssh(args.ssh, args.ssh.cwd, args.target, args.arguments, undefined, false, args.autorun || []).then(() => {
+				this.sendResponse(response);
+			}, err => {
+				this.sendErrorResponse(response, 106, `Failed to SSH: ${err.toString()}`);
+			});
+		} else {
+			this.miDebugger.load(args.cwd, args.target, args.arguments, undefined, args.autorun || []).then(() => {
+				this.sendResponse(response);
+			}, err => {
+				this.sendErrorResponse(response, 107, `Failed to load MI Debugger: ${err.toString()}`);
+			});
+		}
+	}
+
+	protected override attachRequest(response: DebugProtocol.AttachResponse, args: AttachRequestArguments): void {
+		const dbgCommand = args.aliceserverpath || "lldb-mi";
+		if (this.checkCommand(dbgCommand)) {
+			this.sendErrorResponse(response, 104, `Configured debugger ${dbgCommand} not found.`);
+			return;
+		}
+		this.miDebugger = new MI2_ALICE(dbgCommand, [], args.debugger_args, args.env);
+		this.setPathSubstitutions(args.pathSubstitutions);
+		this.initDebugger();
+		this.quit = false;
+		this.attached = true;
+		this.initialRunCommand = args.stopAtConnect ? RunCommand.NONE : RunCommand.CONTINUE;
+		this.isSSH = false;
+		this.setValuesFormattingMode(args.valuesFormatting);
+		this.miDebugger.printCalls = !!args.printCalls;
+		this.miDebugger.debugOutput = !!args.showDevDebugOutput;
+		this.stopAtEntry = args.stopAtEntry;
+		this.miDebugger.attach(args.cwd, args.executable, args.target, args.autorun || []).then(() => {
+			this.sendResponse(response);
+		}, err => {
+			this.sendErrorResponse(response, 108, `Failed to attach: ${err.toString()}`);
+		});
+	}
+
+	// Add extra commands for source file path substitution in LLDB-specific syntax
+	protected setPathSubstitutions(substitutions: { [index: string]: string }): void {
+		if (substitutions) {
+			Object.keys(substitutions).forEach(source => {
+				this.miDebugger.extraCommands.push("settings append target.source-map " + source + " " + substitutions[source]);
+			});
+		}
+	}
+}
+
+DebugSession.run(AliceserverDebugSession);
---
+++ src/backend/mi2/mi2aliceserver.ts
@@ -0,0 +1,75 @@
+import { MI2, escape } from "./mi2";
+import { Breakpoint } from "../backend";
+import * as ChildProcess from "child_process";
+import * as path from "path";
+import { MINode } from "../mi_parse";
+
+export class MI2_ALICE extends MI2 {
+	protected override initCommands(target: string, cwd: string, attach: boolean = false) {
+		// We need to account for the possibility of the path type used by the debugger being different
+		// than the path type where the extension is running (e.g., SSH from Linux to Windows machine).
+		// Since the CWD is expected to be an absolute path in the debugger's environment, we can test
+		// that to determine the path type used by the debugger and use the result of that test to
+		// select the correct API to check whether the target path is an absolute path.
+		const debuggerPath = path.posix.isAbsolute(cwd) ? path.posix : path.win32;
+
+		if (!debuggerPath.isAbsolute(target))
+			target = debuggerPath.join(cwd, target);
+
+		const cmds = [
+			// Aliceserver is already async by default
+			//this.sendCommand("gdb-set target-async on"),
+			new Promise(resolve => {
+				this.sendCommand("list-features").then(done => {
+					this.features = done.result("features");
+					resolve(undefined);
+				}, err => {
+					this.features = [];
+					resolve(undefined);
+				});
+			}) as Thenable<MINode>
+		];
+		if (!attach)
+			cmds.push(this.sendCommand("file-exec-and-symbols \"" + escape(target) + "\""));
+		for (const cmd of this.extraCommands) {
+			cmds.push(this.sendCliCommand(cmd));
+		}
+		return cmds;
+	}
+
+	override attach(cwd: string, executable: string, target: string, autorun: string[]): Thenable<any> {
+		return new Promise((resolve, reject) => {
+			const args = this.preargs.concat(this.extraargs || []);
+			this.process = ChildProcess.spawn(this.application, args, { cwd: cwd, env: this.procEnv });
+			this.process.stdout.on("data", this.stdout.bind(this));
+			this.process.stderr.on("data", this.stderr.bind(this));
+			this.process.on("exit", () => this.emit("quit"));
+			this.process.on("error", err => this.emit("launcherror", err));
+			const promises = this.initCommands(target, cwd, true);
+			promises.push(this.sendCommand("file-exec-and-symbols \"" + escape(executable) + "\""));
+			promises.push(this.sendCommand("attach " + target));
+			promises.push(...autorun.map(value => { return this.sendUserInput(value); }));
+			Promise.all(promises).then(() => {
+				this.emit("debug-ready");
+				resolve(undefined);
+			}, reject);
+		});
+	}
+
+	override setBreakPointCondition(bkptNum: number, condition: string): Thenable<any> {
+		return this.sendCommand("break-condition " + bkptNum + " \"" + escape(condition) + "\" 1");
+	}
+
+	override goto(filename: string, line: number): Thenable<Boolean> {
+		return new Promise((resolve, reject) => {
+			// LLDB parses the file differently than GDB...
+			// GDB doesn't allow quoting only the file but only the whole argument
+			// LLDB doesn't allow quoting the whole argument but rather only the file
+			const target: string = (filename ? '"' + escape(filename) + '":' : "") + line;
+			this.sendCliCommand("jump " + target).then(() => {
+				this.emit("step-other", undefined);
+				resolve(true);
+			}, reject);
+		});
+	}
+}
